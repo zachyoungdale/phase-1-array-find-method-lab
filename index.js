2239077bc31e9a00a8a0edd6b7b026e1a6551d0b
Zachary Youngdale <zyoungdale@gmail.com>
2022-08-31 10:16:07 -0400

    commit tree 4fbde5c365f1ef7d6f95fa018963b526a548ded6

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+  const sadReality =  [ { result: "N/A" }, { result: "N/A" }, { result: "N/A" } ]
+
+
+
+function superbowlWin(array) {
+    let win = array.find(element => element.result === "W") 
+    if (win) {
+        return win.year
+    }
+    return win
+}
+
+
